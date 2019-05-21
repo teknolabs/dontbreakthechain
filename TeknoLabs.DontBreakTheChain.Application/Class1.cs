6693eebc432699ed52498be230baf7a5f1f0c1f8
Fatih CELEN <fatihcelen@gmail.com>
2019-05-22 00:26:45 +0300

    change directory

---
+++ TeknoLabs.DontBreakTheChain.Application/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeknoLabs.DontBreakTheChain.Application
+{
+    class Class1
+    {
+    }
+}
---
+++ TeknoLabs.DontBreakTheChain.Core/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeknoLabs.DontBreakTheChain.Core
+{
+    class Class1
+    {
+    }
+}
